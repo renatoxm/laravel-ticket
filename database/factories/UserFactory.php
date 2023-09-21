641601fb18240e8721f158fdb8b79cbecd94caa2
Renato Nabinger <renatoxm@gmail.com>
2023-09-21 10:42:05 -0300

    fix: user model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,9 +2,35 @@
 
 namespace Renatoxm\LaravelTicket\Database\Factories;
 
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Renatoxm\LaravelTicket\Models\User;
+use Illuminate\Support\Str;
 
-class UserFactory extends \Orchestra\Testbench\Factories\UserFactory
+class UserFactory extends Factory
 {
     protected $model = User::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+            // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    public function unverified(): static
+    {
+        return $this->state(fn(array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+
+    public function modelName()
+    {
+        return $this->model ?? config('auth.providers.users.model', User::class);
+    }
 }
